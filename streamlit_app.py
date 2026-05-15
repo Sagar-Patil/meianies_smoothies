10a2bd6e8a60b3fa76bcf991552eb7a40432a7c1
Sagar-Patil <sagarpatilvp@gmail.com>
2026-05-15 09:00:33 +0000

    S

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.write('The name on your Smoothie will be: ',name_on_order)
 conn =st.connection("snowflake")
 session = conn.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)    
 #st.stop()
 
@@ -41,13 +41,13 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        #st.subheader(fruit_chosen + ' Nutrition Information')
-        #url = "https://my.smoothiefroot.com/api/fruit/watermelon"
-        #response = requests.get(url + fruit_chosen)
-        #sf_df = st.dataframe(data=response.json(),use_container_width=True)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        url = "https://my.smoothiefroot.com/api/fruit/watermelon"
+        response = requests.get(url + fruit_chosen)
+        sf_df = st.dataframe(data=response.json(),use_container_width=True)
     
     #st.write(ingredients_list)
 
